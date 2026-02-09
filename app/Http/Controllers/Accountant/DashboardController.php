af587a553e13777eb4283a46371ef28d33acf8dd
ThilinaSupunsara <thilinasupunsara2001@gmail.com>
2026-02-09 20:34:16 +0530

    up

--- app/Http/Controllers/Accountant/DashboardController.php
+++ app/Http/Controllers/Accountant/DashboardController.php
@@ -27,13 +27,13 @@ class DashboardController extends Controller
                                     ->where('due_date', '<', Carbon::today())
                                     ->sum(DB::raw('total_amount - paid_amount'));
 
-       
+
         $monthlyIncome = Payment::whereYear('payment_date', now()->year)
                                 ->whereMonth('payment_date', now()->month)
                                 ->sum('amount');
 
 
-        // මේ දත්ත 4 view එකට pass කරනවා
+       
         return view('accountant.dashboard', compact(
             'pendingInvoicesCount',
             'totalDueAmount',
